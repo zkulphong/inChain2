fd2027930bfdf43d5b1f4218a73f45ea76ac0799
zkulphongku <zkulphong@ku.edu>
2018-01-28 09:18:49 -0600

    test

--- starter.py
+++ starter.py
@@ -1,6 +1,6 @@
 from run import run
 
-asdf = run({"doctorfirstname": "0", "doctorlastname": "0", "patientfirstname": "0", "patientlastname": "0", "diagnosis": "0", "diagnosisdate": "0", "conditionstart": "0"})
-
 def addBlock(data):
     asdf.parse(data)
+
+asdf = run({"doctorfirstname": "0", "doctorlastname": "0", "patientfirstname": "0", "patientlastname": "0", "diagnosis": "0", "diagnosisdate": "0", "conditionstart": "0"})
